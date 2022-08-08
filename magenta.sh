34bb02e14adb7e14c0ca4a608e9494826cb8d433
oLsen <0lsen@users.noreply.github.com>
2022-08-08 15:07:05 +0200

    Update magenta.sh

--- magenta.sh
+++ magenta.sh
@@ -11,32 +11,18 @@ else
 fi
 
 # define certain URLs / constants
-ASSET_ID_NAME="data-asset-id"
-MANIFEST_URL="https://wcps.t-online.de/bootstrap/magentamusic/v1/manifest"
-MANIFEST_DEVICE_ID="00000000-0000-0000-0000-000000000000"
-MANIFEST_MODEL="mm-web"
-MANIFEST_FIRMWARE="xx"
-MANIFEST_APPVERSION="2000"
-MANIFEST_APPNAME="MagentaMusic"
-MANIFEST_RUNTIMEVERSION="1"
-PLAYER_URL="https://wcps.t-online.de/cvss/magentamusic/vodplayer/v3"
+ASSET_ID_NAME="assetId"
+DM_MOVIE_PREFIX="DMM_MOVIE_"
+PLAYER_URL="https://wcps.t-online.de/cvss/magentamusic/vodclient/v2/player/58935"
 MOVIE_URL="https://wcps.t-online.de/cmrs/magentamusic/media/v1/hlsstreaming/movie"
 
 # fetch IDs and ultimately playlist
 echo -e "\nfetching Asset ID...     (from $URL)"
-ASSET_ID=$(curl $URL 2>&1 | grep -Eo "$ASSET_ID_NAME=\"\d+" | grep -Eo "\d+")
+ASSET_ID=$(curl $URL 2>&1 | grep -Eo "$ASSET_ID_NAME\":\"$DM_MOVIE_PREFIX\d+" | grep -Eo "\d+")
 echo "Asset ID: $ASSET_ID"
 
-echo -e "\nfetching Detail ID...     (from $MANIFEST_URL?deviceId=$MANIFEST_DEVICE_ID&model=$MANIFEST_MODEL&firmware=$MANIFEST_FIRMWARE&appVersion=$MANIFEST_APPVERSION&appName=$MANIFEST_APPNAME&runtimeVersion=$MANIFEST_RUNTIMEVERSION)"
-DETAIL_ID=$(curl "$MANIFEST_URL?deviceId=$MANIFEST_DEVICE_ID&model=$MANIFEST_MODEL&firmware=$MANIFEST_FIRMWARE&appVersion=$MANIFEST_APPVERSION&appName=$MANIFEST_APPNAME&runtimeVersion=$MANIFEST_RUNTIMEVERSION" 2>&1 | grep -Eo "\"value\":\s*\"$PLAYER_URL/details/\d{2,}" | grep -Eo "\d{2,}")
-echo "Detail ID: $DETAIL_ID"
-
-echo -e "\nfetching Player ID...     (from $PLAYER_URL/details/$DETAIL_ID/$ASSET_ID)"
-PLAYER_ID=$(curl "$PLAYER_URL/details/$DETAIL_ID/$ASSET_ID" 2>&1 | grep -Eo "$PLAYER_URL/player/\d+" | grep -Eo "\d+$")
-echo "Player ID: $PLAYER_ID"
-
-echo -e "\nfetching Content ID...     (from $PLAYER_URL/player/$PLAYER_ID/$ASSET_ID/Main%20Movie)"
-CONTENT_ID=$(curl "$PLAYER_URL/player/$PLAYER_ID/$ASSET_ID/Main%20Movie" 2>&1 | grep -Eo "\"contentNumber\":\s*\"\d+" | grep -Eo "\d+")
+echo -e "\nfetching Content ID...     (from $PLAYER_URL/$ASSET_ID/Main%20Movie)"
+CONTENT_ID=$(curl "$PLAYER_URL/$ASSET_ID/Main%20Movie" 2>&1 | grep -Eo "$MOVIE_URL/$ASSET_ID/\d+" | grep -Eo "\d+$")
 echo "Content ID: $CONTENT_ID"
 
 echo -e "\nfetching Playlist URL...     (from $MOVIE_URL/$ASSET_ID/$CONTENT_ID)"
